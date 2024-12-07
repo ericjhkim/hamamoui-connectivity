060d86dd0423e3295f6659a39f3c4ebb073b0e70
ericjhkim <erickim2010@hotmail.com>
2024-12-07 17:41:37 -0500

    N=9

--- data_viewer.py
+++ data_viewer.py
@@ -4,7 +4,7 @@ N_list = []
 P_THRESH_list = []
 pr_list = []
 
-paths = ["data_20241204_150855_5_10.h5","data_20241204_123442_6_10.h5","data_20241204_115953_7_10.h5","data_20241204_183635_8_10.h5"]
+paths = ["data_20241204_150855_5_10.h5","data_20241204_123442_6_10.h5","data_20241204_115953_7_10.h5","data_20241204_183635_8_10.h5","data_20241205_071416_9_10.h5"]
 
 for path in paths:
     N, P_THRESH, phis, matching_frequencies, pr = tools.load_data(f"data/{path}")
